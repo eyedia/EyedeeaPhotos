bb0f3f3285cc4769823e1bff0670b1f5945ebfbf
Debjyoti Das <debjyoti@debjyoti.com>
2025-01-31 14:41:59 -0500

    fixes

--- config_log.js
+++ config_log.js
@@ -1,10 +1,15 @@
 import winston from 'winston';
+import moment from 'moment-timezone';
 
 const logger = winston.createLogger({
   level: 'info', // Log level (e.g., error, warn, info, debug)
   format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
+    winston.format.timestamp({
+      format: () => moment().tz('America/New_York').format('YYYY-MM-DD HH:mm:ss z') 
+    }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
   ),
   transports: [
     new winston.transports.File({ filename: './logs/logs.log' }),

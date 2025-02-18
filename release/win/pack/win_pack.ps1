2d2d846b45e7fb9d84794d1d2b699940eab89e85
Debjyoti Das <debjyoti@debjyoti.com>
2025-02-18 16:08:09 -0500

    pack

--- release/win/pack/win_pack.ps1
+++ release/win/pack/win_pack.ps1
@@ -6,13 +6,13 @@ $proc = Start-Process $command $arguments -NoNewWindow -PassThru
 $proc.WaitForExit()
 
 
-$files_to_delete = ".gitignore", "babel.config.js", "jest.config.js", "package-lock.json", "graphics", "tests", ".git", ".github", "ps_commands.txt"
+$files_to_delete = ".gitignore", "babel.config.js", "jest.config.js", "package-lock.json", "graphics", "tests", ".git", ".github", "ps_commands.txt", "release\win\pack"
 foreach ($file in $files_to_delete) {
     Remove-Item -Path $PSScriptRoot\EyedeeaPhotos\$file -Recurse -ErrorAction SilentlyContinue -Force -Confirm:$false
 }
 
 $install_folder = Get-Item $PSScriptRoot
-$zip_file = $install_folder.Parent.FullName
+$zip_file = $install_folder.Parent.Parent.FullName
 
-.\win_zip.ps1 -input_folder $PSScriptRoot\EyedeeaPhotos -zip_file $zip_file\install\src.zip
+.\win_zip.ps1 -input_folder $PSScriptRoot\EyedeeaPhotos -zip_file $zip_file\win\install\EyedeeaPhotos.zip
 

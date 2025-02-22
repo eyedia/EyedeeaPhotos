43ca3f4b925cba70c26c6ec0f1a360e85fbe6894
Debjyoti Das <debjyoti@debjyoti.com>
2025-02-22 09:11:30 -0500

    setup

--- release/win/install/win_setup.ps1
+++ release/win/install/win_setup.ps1
@@ -1,28 +1,11 @@
 
 $eyedeea_url = "http://127.0.0.1:8080"
-#currently we have hardcoded source id, we will create NAS by ourselves to lock id 1
-try {
-    $body_raw = [PSCustomObject]@{
-        name = "nas"
-        url = "http://192.168.90.101:5001/webapi"
-        user = "eyedeea_player"
-        password = "password"
-    }
-    $body = ConvertTo-Json $body_raw
-    $response = Invoke-RestMethod -Uri $eyedeea_url"/api/sources" -Method Post -Body $body -ContentType "application/json"
-    # Write-Host "Response:"
-    # $response | ConvertTo-Json
-}
-catch {
-    Write-Host "Error:"
-    Write-Host $_.Exception.Message
-}
 
 $source_type = Read-Host -Prompt "Enter source type(nas/fs)"
 if(($source_type -ne "nas") -or ($source_type -ne "nas")){
     $source_type = "fs"
 }
-# $source_name = Read-Host -Prompt "Enter source name:"
+$source_name = Read-Host -Prompt "Enter source name"
 $url_prompt = "Enter photo repository (e.g. D:\photos)"
 if($source_type -eq "nas"){
     $url_prompt = "Enter Synlogy NAS url (e.g.https://192.168.86.218:5001/webapi)"
@@ -39,15 +22,25 @@ if($source_type -eq "nas"){
 
 try {
     $body_raw = [PSCustomObject]@{
-        name = $source_type
+        name = $source_name
+        type = $source_type
         url = $source_url 
         user = $source_user
         password = $source_password
     }
     $body = ConvertTo-Json $body_raw
     $response = Invoke-RestMethod -Uri $eyedeea_url"/api/sources" -Method Post -Body $body -ContentType "application/json"
-    Write-Host "Response:"
-    $response | ConvertTo-Json
+    
+    $source_id = $response.id
+    Write-Host "New source registered. id": $source_id
+
+    $yes_no = Read-Host -Prompt "Do you want to start scan (y/n)?"
+    $uri = $eyedeea_url + "/api/sources/" + $source_id + "/scan"
+    Write-Host $uri
+    if($yes_no -eq "y"){
+        $response = Invoke-RestMethod -Uri $uri -Method Post
+        Write-Host $response
+    }
 }
 catch {
     Write-Host "Error:"

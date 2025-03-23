2206aea07f94925653d73d00d9e4dbac5635c5a5
Debjyoti Das <debjyoti@debjyoti.com>
2025-03-23 00:02:38 -0400

    fixes

--- tests/api/api.int.test.js
+++ tests/api/api.int.test.js
@@ -7,7 +7,7 @@ afterAll((done) => {
 
 describe('Server Status', () => {
   it('Check server status', async () => {
-    const response = await request(server.server).get('/status');
+    const response = await request(server.server).get('/api/system/status');
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ status: 'up' });
   });

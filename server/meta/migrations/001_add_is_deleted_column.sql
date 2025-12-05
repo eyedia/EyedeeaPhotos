7dbe6bdd2d3fe09d856095c753df912b12ef7e71
Debjyoti Das <debjyoti@debjyoti.com>
2025-12-05 11:40:01 -0500

    fixes

--- server/meta/migrations/001_add_is_deleted_column.sql
+++ server/meta/migrations/001_add_is_deleted_column.sql
@@ -1,13 +1,8 @@
 -- Add is_deleted column to source table for soft delete functionality
--- This migration adds support for marking sources as deleted without removing data
 
--- Begin transaction for safe execution
-BEGIN TRANSACTION;
-
--- Add column if it doesn't exist (will only work if checked in migration manager)
 ALTER TABLE source ADD COLUMN is_deleted INTEGER DEFAULT 0;
 
 -- Create index for faster queries filtering deleted sources
 CREATE INDEX IF NOT EXISTS idx_source_is_deleted ON source(is_deleted);
 
-COMMIT;
+
